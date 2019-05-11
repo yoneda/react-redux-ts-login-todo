1ead15e0987e114f56dd6be1de2030f6c1692b97
yoneda <nsshowdream@gmail.com>
2019-05-11 23:58:19 +0900

    remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import SimpleTab from "./components/SimpleTab";
 import Tasks from "./components/Tasks";
 import TaskElement from './components/TaskElement';
 import YourTaskElement from "./components/YourTaskElement";
-import TaskInput from "./components/TaskInput";
 import './App.css';
 
 const initialTasks = () => [
