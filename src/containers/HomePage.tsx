3be3a5bb4847804e3d4d55ca4440a627e7c6050d
yoneda <nsshowdream@gmail.com>
2019-05-12 02:04:01 +0900

    add Header Component

--- src/containers/HomePage.tsx
+++ src/containers/HomePage.tsx
@@ -2,9 +2,40 @@ import React from 'react';
 import Header from "../components/Header";
 import Title from "../components/Title";
 import Line from "../components/Line";
+import SimpleTab from "../components/SimpleTab";
+import Tasks from "../components/Tasks";
+import TaskElement from '../components/TaskElement';
+import YourTaskElement from "../components/YourTaskElement";
 import { Link } from "react-router-dom";
 
+const initialTasks = () => [
+  { id: 0, name: "clean my room", done: false },
+  { id: 1, name: "sleep a lot", done: false },
+  { id: 2, name: "learn programming", done: false },
+]
+
+const otherTasks = () => [
+  { id: 0, name: "read a book", done: false, user: "yoneda" },
+  { id: 1, name: "clean my window", done: false, user: "yoneda" },
+  { id: 2, name: "write texts", done: true, user: "tanaka" },
+]
+
 const HomePage: React.FC = () => {
+  const [tasks, setTasks] = React.useState(initialTasks);
+  const renderPrivateTasks = () => tasks.map((task, key) => (
+    <YourTaskElement
+      {...task}
+      key={task.id}
+      doneHandler={() => setTasks(tasks.map(doneTask => doneTask.id === task.id ? { ...doneTask, done: !doneTask.done } : doneTask))}
+      removeHandler={() => setTasks(tasks.filter(removeTask => removeTask.id !== task.id))}
+    />
+  ))
+  const renderGlobalTasks = () => otherTasks().map(task => (
+    <TaskElement
+      {...task}
+      key={task.id}
+    />
+  ))
   return (
     <div>
       <Header>
@@ -14,6 +45,13 @@ const HomePage: React.FC = () => {
         <Link to="login">sign in</Link>
         <Line />
       </Header>
+      <SimpleTab
+        titles={["private", "global"]}
+        elements={[
+          <Tasks>{renderPrivateTasks()}</Tasks>,
+          <Tasks>{renderGlobalTasks()}</Tasks>
+        ]}
+      />
     </div>
   )
 }
