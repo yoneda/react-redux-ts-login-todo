5cd8d72d4e56b3c8c2b4c8ba1099ba59f16fbe92
yoneda <nsshowdream@gmail.com>
2019-05-17 00:19:12 +0900

    add RegisterForm

--- src/containers/RegisterPage.tsx
+++ src/containers/RegisterPage.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import Header from "../components/Header";
 import Title from "../components/Title";
 import Line from "../components/Line";
+import RegisterForm from "../components/RegisterForm";
+import Button from "../components/Button";
 import { Link } from "react-router-dom";
+import FormInput from '../components/FormInput';
 
 const RegisterPage: React.FC = () => {
   return (
@@ -14,6 +17,12 @@ const RegisterPage: React.FC = () => {
         <Link to="login">sign in</Link>
         <Line />
       </Header>
+      <RegisterForm
+        renderForm1={(mail, setMail) => <FormInput text={mail} placeholder={"mail"} changeHandler={(text) => setMail(text)} />}
+        renderForm2={(user, setUser) => <FormInput text={user} placeholder={"user"} changeHandler={(text) => setUser(text)} />}
+        renderForm3={(pass, setPass) => <FormInput text={pass} placeholder={"pass"} changeHandler={(text) => setPass(text)} />}
+        renderButton={(mail, user, pass) => <Button text={"sign in"} clickHandler={() => console.log(`mail:${mail},user: ${user},pass: ${pass}`)} />}
+      />
     </div>
   )
 }
